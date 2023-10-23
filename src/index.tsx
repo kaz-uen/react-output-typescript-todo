951d9658af8c171ff56af26546989943867ef81e
Kaz Ueno <kazun.ue@gmail.com>
2023-10-23 22:41:51 +0900

    createGlobalStyleでグローバルスタイルを設定

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './global.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './store/store';
+import GlobalStyle from './GlobalCss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+        <GlobalStyle />
         <App />
       </Provider>
     </BrowserRouter>
