1811c5bb6918eeaf0b9bcec8e3d42fef106a1352
Kaz Ueno <kazun.ue@gmail.com>
2023-11-17 03:52:34 +0900

    Todo slice 修正

--- src/features/todo/slice.ts
+++ src/features/todo/slice.ts
@@ -8,8 +8,6 @@ const TodoSlice = createSlice({
   reducers: {
     addTodo(state, action): void {
       const getMaxTodoId = (): number => Math.max(...state.todoItemsData.map((item) => item.id));
-      // Math.maxの引数にはスプレッド構文で値を渡す（配列で渡すと動かない）
-      // 参考：https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/max
       state.todoItemsData.push({
         id: state.amount !== 0 ? getMaxTodoId() + 1 : INITIAL_TODO_ID,
         title: action.payload.title,
@@ -30,23 +28,6 @@ const TodoSlice = createSlice({
         return { ...item };
       });
     },
-
-    save(state, action) {
-      if (!action.payload.id) {
-        const id = Math.max(...state.todoItemsData.map((item) => item.id));
-        state.todoItemsData.push({
-          ...action.payload,
-          id
-        });
-
-        return;
-      }
-
-      const index = state.todoItemsData.findIndex((it) => it.id === action.payload.id);
-      if (index >= 0) {
-        state.todoItemsData[index] = action.payload;
-      }
-    },
     deleteTodo(state, action): void {
       state.todoItemsData = state.todoItemsData.filter((item) => (item.id !== action.payload ? true : false));
       state.amount = state.todoItemsData.length;
@@ -60,6 +41,5 @@ const TodoSlice = createSlice({
   }
 });
 
-// export const { addTodo, updateTodo, deleteTodo, clearTodo, searchTodo } = TodoSlice.actions;
-export const { addTodo, updateTodo, deleteTodo, clearTodo, searchTodo, save } = TodoSlice.actions;
+export const { addTodo, updateTodo, deleteTodo, clearTodo, searchTodo } = TodoSlice.actions;
 export default TodoSlice.reducer;
